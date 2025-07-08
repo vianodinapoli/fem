56eb791b455bab32148bad77e6e1ea1404a1a590
Viano Di Napoli <vianodinapoli@gmail.com>
2025-07-08 19:55:22 +0200

    teste

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[{has:void 0,source:"\u002Fapi\u002Fregistos",destination:"\u002Fapi\u002F__registos"}],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
